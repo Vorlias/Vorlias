f89a19423d92b3a59845c1ec77b7c6008b49b357
Jonathan Holmes <Vorlias@users.noreply.github.com>
2022-06-30 14:16:12 +1200

    Update fetch-assets.sh

--- fetch-assets.sh
+++ fetch-assets.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-IslandsIconUrl=$(curl https://thumbnails.roblox.com/v1/places/gameicons\?placeIds\=4872321990\&size\=512x512\&format\=Png\&isCircular\=false | jq -j '.data[0].imageUrl')
+IslandsIconUrl=$(curl https://thumbnails.roblox.com/v1/places/gameicons\?placeIds\=4872321990\&size\=256x256\&format\=Png\&isCircular\=false | jq -j '.data[0].imageUrl')
 curl $IslandsIconUrl > assets/islands-icon.png
