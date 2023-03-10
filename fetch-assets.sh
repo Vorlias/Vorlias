6859b1aef6a904e60831c065486d0955e8954ca0
Jonathan Holmes <Vorlias@users.noreply.github.com>
2023-03-10 22:23:33 +1300

    Update fetch-assets.sh

--- fetch-assets.sh
+++ fetch-assets.sh
@@ -2,5 +2,5 @@
 IslandsIconUrl=$(curl https://thumbnails.roblox.com/v1/places/gameicons\?placeIds\=4872321990\&size\=256x256\&format\=Png\&isCircular\=false | jq -j '.data[0].imageUrl')
 curl $IslandsIconUrl > assets/islands-icon.png
 
-ZenerithIconUrl=$(curl https://thumbnails.roblox.com/v1/places/gameicons\?placeIds\=6191725593\&size\=256x256\&format\=Png\&isCircular\=false | jq -j '.data[0].imageUrl')
+ZenerithIconUrl=$(curl https://thumbnails.roblox.com/v1/places/gameicons\?placeIds\=6889068401\&size\=256x256\&format\=Png\&isCircular\=false | jq -j '.data[0].imageUrl')
 curl $ZenerithIconUrl > assets/zenerith-icon.png
